1d63b03f4b789c9bf617a98022002636ceccb4f3
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-23 14:02:20 +0530

    Adding Product Review Management to remote

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            //Collection initializer
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                new ProductReview(){ ProductId=1,UserId=1,Review="good",Rating=17,IsLike=true},
+                new ProductReview(){ ProductId=2,UserId=3,Review="bad",Rating=1,IsLike=false},
+                new ProductReview(){ ProductId=3,UserId=5,Review="good",Rating=20,IsLike=true},
+                new ProductReview(){ ProductId=4,UserId=7,Review="average",Rating=10,IsLike=true},
+                new ProductReview(){ ProductId=5,UserId=1,Review="bad",Rating=5,IsLike=false}
+            };
+        }
+    }
+}
