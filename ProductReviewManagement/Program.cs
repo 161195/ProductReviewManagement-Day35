a6d9018861baa508ebc2d33274984dfc88f7a9fb
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-25 12:22:30 +0530

    Using groupby method counting ID

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -15,7 +15,7 @@ namespace ProductReviewManagement
             List<ProductReview> list = new List<ProductReview>()
             {
                 new ProductReview(){ ProductId=1,UserId=1,Review="good",Rating=17,IsLike=true},
-                new ProductReview(){ ProductId=2,UserId=3,Review="bad",Rating=1,IsLike=false},
+                new ProductReview(){ ProductId=1,UserId=3,Review="bad",Rating=1,IsLike=false},
                 new ProductReview(){ ProductId=3,UserId=5,Review="good",Rating=20,IsLike=true},
                 new ProductReview(){ ProductId=4,UserId=7,Review="average",Rating=10,IsLike=true},
                 new ProductReview(){ ProductId=5,UserId=1,Review="bad",Rating=5,IsLike=false}
@@ -25,6 +25,9 @@ namespace ProductReviewManagement
             Console.WriteLine("Records based on rating and product id : ");
             RetrieveRecordsBasedOnRatingAndProductId(list);
             Console.ReadLine();
+            Console.WriteLine("Count of ProductId : ");
+            CountingProductID(list);
+
         }
         //UC2
         //This method for retrieve top three records from list
@@ -49,6 +52,17 @@ namespace ProductReviewManagement
                 Console.WriteLine("ProductId : " + element.ProductId + " Rating : " + element.Rating + " UserId : " + element.UserId + " Review : " + element.Review + " IsLike : " + element.IsLike);
             }
         }
-
+        //UC-4
+        //counting each ID present in the list
+        public static void CountingProductID(List<ProductReview> list)
+        {
+            //Method syntax for LINQ
+            var data = list.GroupBy(p => p.ProductId).Select(x => new { ProductId = x.Key, count = x.Count() }); 
+            foreach (var element in data)
+            {
+                Console.WriteLine("ProductId : " + element.ProductId + " \t " + "Count" + element.count);
+                Console.WriteLine("-----------------------");
+            }
+        }
     } 
 }
